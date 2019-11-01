279c3f2616d12e62f08bdf4fd0a3e3772f93ff25
NGeorgiev <NGeorgiev@spartaglobal.com>
2019-11-01 09:42:45 +0000

    initial commit

---
+++ src/main/java/com/spartaglobal/JavaDatabaseConnections/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.JavaDatabaseConnections;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
